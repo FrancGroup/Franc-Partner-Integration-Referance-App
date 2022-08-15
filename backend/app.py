8ffb639ca7c5e51c7f28d6c0ec8aa23f3c2f1fac
Dylan Solin <dylan@hifranc.com>
2022-08-15 15:55:32 +0200

    getBreakoutURL endpoint

--- backend/app.py
+++ backend/app.py
@@ -81,11 +81,9 @@ def register():
     }, 200)
 
 
-@app.route("/getBreakoutURL")
+@app.route("/getBreakoutURL", methods = ["GET"])
 def get_breakout_url():
-    data = request.json
-    payload = jwt.decode(data["token"], JWT_SCT, JWT_ALG)
-    return jsonify({"url": "https://"})
+    return jsonify({"url": get_breakout_url()})
 
 
 @app.route("/register_callback")
