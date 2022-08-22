957abcc6ef0a4962334b57e54b4f211c24e52c0f
Dylan Solin <dylan@hifranc.com>
2022-08-22 10:55:49 +0200

    Sign Up and Register Screen

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -2,13 +2,14 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:franc_third_party_integration_demo/home_screen.dart';
+import 'package:franc_third_party_integration_demo/login_screen.dart';
 import 'package:franc_third_party_integration_demo/web_view_app_screen.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: HomeScreen(),
+      home: LoginScreen(),
     ),
   );
 }
---
+++ mobile_app/lib/registration_screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:franc_third_party_integration_demo/login_screen.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({Key? key}) : super(key: key);
+
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: ListView(
+            children: <Widget>[
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'Franc Third Party Integration',
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 30),
+                  )),
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'Register',
+                    style: TextStyle(fontSize: 20),
+                  )),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  controller: usernameController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Username',
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                child: TextField(
+                  obscureText: true,
+                  controller: passwordController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                  height: 50,
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: ElevatedButton(
+                    child: const Text('Register'),
+                    onPressed: () {
+                      ((usernameController.text != '') &&
+                              (passwordController.text != ''))
+                          ? handleRegister()
+                          : null;
+                    },
+                  )),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          )),
+    );
+  }
+
+  void handleRegister() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const LoginScreen()),
+    );
+  }
+}
